cd434c4dfcfcdbcdcc3238416121dc5506cf9d84
Keksi <contact@keksi.me>
2024-02-20 11:16:28 +0100

    Add User Container and Content Container

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), icon()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{astro, ts,tsx}',
+    './components/**/*.{astro, ts,tsx}',
+    './app/**/*.{astro, ts,tsx}',
+    './src/**/*.{astro, ts,tsx}',
   ],
   prefix: "",
   theme: {
