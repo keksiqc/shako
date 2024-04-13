c2a4345fd5cb47b1929ac0d770c337cc0e21fc95
Keksi <contact@keksi.me>
2024-04-13 14:29:52 +0200

    chore: fix formating

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
 interface IconButton {
-  icon: string;
-  url: string;
+  icon: string
+  url: string
 }
 
 interface Button {
-  name: string;
-  icon: string;
-  url: string;
+  name: string
+  icon: string
+  url: string
 }
 
 export interface Config {
-  discordId: string;
-  lanyardUrl: string;
-  iconButtons: IconButton[];
-  buttons: Button[];
+  discordId: string
+  lanyardUrl: string
+  iconButtons: IconButton[]
+  buttons: Button[]
 }
