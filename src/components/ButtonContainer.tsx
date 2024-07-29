b95067e530b862f0f18d4ee7fc095fd72ab8a004
Keksi <contact@keksi.me>
2024-07-30 01:33:14 +0200

    chore: bump deps

--- src/components/ButtonContainer.tsx
+++ src/components/ButtonContainer.tsx
@@ -9,12 +9,15 @@ export function ButtonContainer() {
       {/* icon buttons */}
       <div className="flex items-center justify-center gap-3">
         {config.iconButtons.map((button, index) => (
-          <Button variant="secondary" size="icon" key={index}>
+          <Button
+            variant="secondary"
+            size="icon"
+            key={`${button.icon}-${index}`}
+          >
             <Icon
               icon={`simple-icons:${button.icon}`}
               className="size-5"
               aria-label={button.icon}
-              key={index}
             />
           </Button>
         ))}
@@ -26,7 +29,7 @@ export function ButtonContainer() {
           variant="outline"
           size="xl"
           onClick={() => window.open(button.url)}
-          key={index}
+          key={`${button.name}-${index}`}
           aria-label={button.name}
         >
           <Icon icon={`simple-icons:${button.icon}`} className="mr-2 size-5" />{" "}
