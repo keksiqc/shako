07c0ef402e47405b2cb7570ddcb06c49f67d509e
Keksi <contact@keksi.me>
2024-07-29 18:52:55 +0200

    fix: add key to button

--- src/components/ButtonContainer.tsx
+++ src/components/ButtonContainer.tsx
@@ -8,19 +8,20 @@ export function ButtonContainer() {
     <section className="flex flex-col gap-3">
       {/* icon buttons */}
       <div className="flex items-center justify-center gap-3">
-        {config.iconButtons.map((button) => (
-          <Button variant="secondary" size="icon">
-            <Icon icon={`simple-icons:${button.icon}`} className="size-5" aria-label={button.icon} />
+        {config.iconButtons.map((button, index) => (
+          <Button variant="secondary" size="icon" key={index}>
+            <Icon icon={`simple-icons:${button.icon}`} className="size-5" aria-label={button.icon} key={index} />
           </Button>
         ))}
       </div>
 
       {/* buttons */}
-      {config.buttons.map((button) => (
+      {config.buttons.map((button, index) => (
         <Button
           variant="outline"
           size="xl"
           onClick={() => window.open(button.url)}
+          key={index}
           aria-label={button.name}
         >
           <Icon icon={`simple-icons:${button.icon}`} className="mr-2 size-5" />{" "}
