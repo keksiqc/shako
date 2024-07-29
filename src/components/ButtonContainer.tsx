3bc293e5b4dc36813c74abc1a3cef0524ba935ae
Keksi <contact@keksi.me>
2024-07-29 18:43:22 +0200

    feat: add aria labels

--- src/components/ButtonContainer.tsx
+++ src/components/ButtonContainer.tsx
@@ -10,7 +10,7 @@ export function ButtonContainer() {
       <div className="flex items-center justify-center gap-3">
         {config.iconButtons.map((button) => (
           <Button variant="secondary" size="icon">
-            <Icon icon={`simple-icons:${button.icon}`} className="size-5" />
+            <Icon icon={`simple-icons:${button.icon}`} className="size-5" aria-label={button.icon} />
           </Button>
         ))}
       </div>
@@ -21,6 +21,7 @@ export function ButtonContainer() {
           variant="outline"
           size="xl"
           onClick={() => window.open(button.url)}
+          aria-label={button.name}
         >
           <Icon icon={`simple-icons:${button.icon}`} className="mr-2 size-5" />{" "}
           {button.name}
