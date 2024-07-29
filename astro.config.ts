4b08a4d1ff49ac5db6302b6e2466cfe45c53381d
Keksi <contact@keksi.me>
2024-07-30 01:35:31 +0200

    chore: replace analytics with vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,13 @@
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 })
