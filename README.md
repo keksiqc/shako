a01b39d36edcb72582d664ba07fc06ab1a4cd8ad
Keksi <contact@keksi.me>
2024-07-29 18:36:38 +0200

    chore: fix formatting

--- README.md
+++ README.md
@@ -27,7 +27,6 @@ npm run dev
 Edit the `src/config.ts` file to add your links.
 The `icon` prop corresponds to the icon name from [Simple Icons](https://simpleicons.org/). Ensure the icon name matches exactly with the one listed on the Simple Icons website for proper rendering.
 
-
 > [!NOTE]
 > Please follow the official [Astro documentation](https://docs.astro.build/en/guides/deploy/) if you want to deploy this project.
 
--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: cloudflare()
-});
+  adapter: cloudflare(),
+})
