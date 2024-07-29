3a130833f4501ae595057f5974098acfef6d6e84
Keksi <contact@keksi.me>
2024-07-29 18:31:44 +0200

    chore: remove unused deps

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,8 @@
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
-import icon from "astro-icon"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
+  integrations: [react(), tailwind()],
 })
