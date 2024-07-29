9c73fb0ea3756be4514313e3d2f767d968b6a8e2
Keksi <contact@keksi.me>
2024-07-29 18:32:42 +0200

    chore: add cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,12 @@
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from "astro/config"
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-})
+  output: "server",
+  adapter: cloudflare()
+});
