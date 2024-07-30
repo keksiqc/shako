5ccf85bec1b62c4e8cca0a3a924a7c0f118eb330
Keksi <contact@keksi.me>
2024-07-30 13:06:54 +0200

    fix: icon size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="bottom-0 my-12 flex w-full flex-col items-center justify-center gap-2 text-sm text-muted-foreground">
       <div className="flex items-center justify-center gap-1">
-        <Code className="pt-1" /> with <Heart className="pt-1" />
+        <Code size={20} /> with <Heart size={20} />
         by
         <a
           className="ml-1 rounded bg-muted px-[0.3rem] py-[0.2rem] hover:underline"
