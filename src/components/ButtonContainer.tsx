607d5fe71ab80b21d9aba8bc567949f9d78445c5
Keksi <contact@keksi.me>
2024-07-31 14:25:42 +0200

    fix: error when list is empty

--- src/components/ButtonContainer.tsx
+++ src/components/ButtonContainer.tsx
@@ -8,7 +8,7 @@ export function ButtonContainer() {
     <section className="flex flex-col gap-3">
       {/* icon buttons */}
       <div className="flex items-center justify-center gap-3">
-        {config.iconButtons.map((button, index) => (
+        {config.iconButtons?.map((button, index) => (
           <Button
             variant="secondary"
             size="icon"
@@ -25,7 +25,7 @@ export function ButtonContainer() {
       </div>
 
       {/* buttons */}
-      {config.buttons.map((button, index) => (
+      {config.buttons?.map((button, index) => (
         <Button
           variant="outline"
           size="xl"
