7066a35d556cc4a0c8a226d4526a6608af6fbcf5
Keksi <contact@keksi.me>
2024-07-31 14:21:54 +0200

    feat: add support to not use lanyard

--- src/components/UserContainer.tsx
+++ src/components/UserContainer.tsx
@@ -5,21 +5,25 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Skeleton } from "@/components/ui/skeleton"
 
 export function UserContainer() {
-  const { data } = useLanyard(config.discordID)
+  let userName = config.user?.name
+  let avatarUrl = config.user?.avatar
+
+  if (!userName || !avatarUrl) {
+    const { data } = useLanyard(config.discordID)
+    userName = data?.discord_user.global_name
+    avatarUrl = `https://cdn.discordapp.com/avatars/${config.discordID}/${data?.discord_user.avatar}.webp?size=256`
+  }
 
   return (
     <section className="m-5 flex flex-col items-center justify-center gap-5">
-      {data?.discord_user.avatar && data?.discord_user.global_name ? (
+      {avatarUrl && userName ? (
         <>
           <Avatar className="size-32">
-            <AvatarImage
-              src={`https://cdn.discordapp.com/avatars/${config.discordID}/${data.discord_user.avatar}.webp?size=256`}
-              alt="@keksiqc"
-            />
+            <AvatarImage src={avatarUrl} alt={userName} />
             <AvatarFallback>U</AvatarFallback>
           </Avatar>
           <h2 className="text-3xl font-semibold text-foreground">
-            {data.discord_user.global_name}
+            {userName}
           </h2>
         </>
       ) : (
