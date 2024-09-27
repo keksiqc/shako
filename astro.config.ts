5e8a045d88c2a8157aa37dc69ba1b76d3d85ed86
Keksi <contact@keksi.me>
2024-09-27 13:14:30 +0200

    chore: replace Vercel adapter with Cloudflare adapter in Astro config to switch deployment target chore: update package.json to include Cloudflare adapter dependency and remove Vercel adapter dependency

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,12 @@
+import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  adapter: cloudflare(),
 })
