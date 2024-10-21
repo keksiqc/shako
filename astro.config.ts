769b0a7cdb2393938efd04a5757592b690e43786
Keksi <contact@keksi.me>
2024-10-21 10:47:06 +0200

    refactor: use static instead of server as not needed

--- astro.config.ts
+++ astro.config.ts
@@ -6,8 +6,4 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 })
