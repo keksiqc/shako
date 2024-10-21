eca7ffdda707e91afcadf5e46b9e7f135d1c270c
Keksi <contact@keksi.me>
2024-10-21 09:37:37 +0200

    refactor: Update eslint.config.js and package.json
    Refactor eslint.config.js and package.json to use the @keksiqc/eslint-config package instead of @antfu/eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,15 @@
-import antfu from '@antfu/eslint-config'
-import { FlatCompat } from '@eslint/eslintrc'
+import keksiqc from '@keksiqc/eslint-config'
 
-// workaround for flat config not being supported yet by eslint-plugin-tailwindcss
-// https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/280
-// how-to from: https://github.com/antfu/eslint-config/issues/431#issuecomment-2014668812
-const customTailwindConfig = new FlatCompat().config({
-  extends: ['plugin:tailwindcss/recommended'],
-  rules: {
-    'tailwindcss/no-custom-classname': 'warn',
-    'tailwindcss/classnames-order': 'error',
-  },
+export default keksiqc({
+  formatters: true,
+  react: true,
+  astro: true,
+  tailwind: true,
+}, {
   settings: {
-    tailwindcss: {
+    tailwind: {
       callees: ['cn', 'cva'],
       config: 'tailwind.config.ts',
     },
   },
 })
-
-// Add the name property to the config so it displays correctly in the eslint config-inspector (https://github.com/eslint/config-inspector)
-// To run the inspector, use `pnpx @eslint/config-inspector` in the root of the project.
-customTailwindConfig[0].name = 'tailwindcss/recommended'
-
-export default antfu({
-  formatters: true,
-  react: true,
-  astro: true,
-}, customTailwindConfig)
