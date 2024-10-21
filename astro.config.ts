9ce79df9762255418c615c144c42fdf12a01ca34
Keksi <contact@keksi.me>
2024-10-21 11:20:57 +0200

    refactor: Remove unused vercel import in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
