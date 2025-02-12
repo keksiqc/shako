5188a74e899a7548379fe8fdf55d7b2c4b37a5dd
Keksi <contact@keksi.me>
2025-02-12 10:04:18 +0100

    feat: add playformCompress integration and update package.json dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,9 @@
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import playformCompress from '@playform/compress'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), playformCompress()],
 })
