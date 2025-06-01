4f2ccf73afd061fb0ff3dbef7f1645f7fbd892a0
Keksi <me@keksi.dev>
2025-06-01 13:09:48 +0200

    feat: reorganize background components and introduce new animated grid, custom, dot, flickering grid, and grid pattern components

--- src/components/backgrounds/grid-pattern.tsx
+++ src/components/backgrounds/grid-pattern.tsx
@@ -1,4 +1,5 @@
 import { useId } from 'react'
+
 import { cn } from '@/lib/utils'
 
 interface GridPatternProps extends React.SVGProps<SVGSVGElement> {
@@ -6,17 +7,19 @@ interface GridPatternProps extends React.SVGProps<SVGSVGElement> {
   height?: number
   x?: number
   y?: number
+  squares?: Array<[x: number, y: number]>
   strokeDasharray?: string
   className?: string
   [key: string]: unknown
 }
 
-export default function GridPattern({
+export function GridPattern({
   width = 40,
   height = 40,
   x = -1,
   y = -1,
-  strokeDasharray,
+  strokeDasharray = '0',
+  squares,
   className,
   ...props
 }: GridPatternProps) {
@@ -26,7 +29,7 @@ export default function GridPattern({
     <svg
       aria-hidden="true"
       className={cn(
-        'pointer-events-none fixed inset-0 z-[-1] size-full stroke-neutral-400/20',
+        'pointer-events-none fixed inset-0 z-[-1] size-full h-full w-full stroke-neutral-400/15',
         className,
       )}
       {...props}
@@ -37,18 +40,31 @@ export default function GridPattern({
           width={width}
           height={height}
           patternUnits="userSpaceOnUse"
-          patternContentUnits="userSpaceOnUse"
           x={x}
           y={y}
         >
           <path
-            d={`M${height} 0L0 0 0 ${width}`}
+            d={`M.5 ${height}V.5H${width}`}
             fill="none"
             strokeDasharray={strokeDasharray}
           />
         </pattern>
       </defs>
       <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id})`} />
+      {squares && (
+        <svg x={x} y={y} className="overflow-visible">
+          {squares.map(([x, y]) => (
+            <rect
+              strokeWidth="0"
+              key={`${x}-${y}`}
+              width={width - 1}
+              height={height - 1}
+              x={x * width + 1}
+              y={y * height + 1}
+            />
+          ))}
+        </svg>
+      )}
     </svg>
   )
 }
