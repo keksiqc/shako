51f47bab57cbedf1e8642edc28f4aca06bb3dcb7
Keksi <me@keksi.dev>
2025-06-01 18:02:31 +0200

    feat: update button component sizes for xl and icon variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -26,8 +26,8 @@ const buttonVariants = cva(
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        xl: 'h-12 rounded-lg px-8 has-[>svg]:px-6',
-        icon: 'size-9',
+        xl: 'h-14 rounded-lg px-8 has-[>svg]:px-6',
+        icon: 'size-10',
       },
     },
     defaultVariants: {
