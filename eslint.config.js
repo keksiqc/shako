6656ef19d22a863467a99447f8a41aa4403dc1d6
Keksi <git@keksi.dev>
2026-02-11 10:00:01 +0100

    chore(deps): add eslint-config-hyoban and update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,13 @@
 // eslint.config.js
-import path from 'node:path'
-import process from 'node:process'
+import { defineConfig } from 'eslint-config-hyoban'
 
-import antfu from '@antfu/eslint-config'
-import tailwind from 'eslint-plugin-tailwindcss'
-
-export default antfu({
+export default defineConfig({
   stylistic: true,
   react: true,
   astro: true,
-  ignores: ['README.md'],
-}, tailwind.configs['flat/recommended'], {
-  rules: {
-    'tailwindcss/no-custom-classname': 'off',
-    'react-hooks-extra/no-direct-set-state-in-use-effect': 'off',
-  },
-  settings: {
-    tailwindcss: {
-      callees: ['cn', 'cva'],
-      // Construct an absolute path to your global.css
-      config: path.join(process.cwd(), 'src/styles/global.css'),
+  tailwindcss: {
+    settings: {
+      entryPoint: 'src/styles/global.css',
     },
   },
 })
