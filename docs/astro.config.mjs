896666c661aa21b99405ed44d22c7465ae6c1477
Keksi <git@keksi.dev>
2026-03-18 10:25:05 +0100

    chore: remove base path from documentation configuration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/docs',
   integrations: [
     starlight({
       title: 'My Docs',
