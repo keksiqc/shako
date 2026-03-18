efa8f23695e8da5748dc77df9596b621c6dd0f8f
Keksi <git@keksi.dev>
2026-03-18 10:01:39 +0100

    chore: set base path for documentation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/docs',
   integrations: [
     starlight({
       title: 'My Docs',
