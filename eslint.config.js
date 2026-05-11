d3cf17f8447b69f0dd8ea15b5eedb263f5f28bf2
Keksi <git@keksi.dev>
2026-05-11 22:54:23 +0200

    feat: integrate eslint-plugin-better-tailwindcss for improved Tailwind CSS linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,20 @@
 import antfu from '@antfu/eslint-config'
+import tailwindcss from 'eslint-plugin-better-tailwindcss'
 
 export default antfu({
   formatters: true,
   astro: true,
+}, {
+  extends: [
+    tailwindcss.configs.recommended,
+  ],
+  rules: {
+    'better-tailwindcss/enforce-consistent-line-wrapping': 'off',
+
+  },
+  settings: {
+    'better-tailwindcss': {
+      entryPoint: 'src/styles/global.css',
+    },
+  },
 })
