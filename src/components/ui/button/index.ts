1108ef666e062cbad7843f1852801dd25fcf474b
Keksi <git@keksi.dev>
2026-06-18 12:57:36 +0200

    style: fix formatting

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -1,3 +1,2 @@
-export { type ButtonVariantProps, buttonVariants } from './button-variants'
-
 export { default as Button } from './button.astro'
+export { type ButtonVariantProps, buttonVariants } from './button-variants'
